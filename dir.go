751e865489b8448c512cdb14ab7c08e00c4da097
Xuanli <ludwigchen@proton.me>
2023-09-22 16:27:13 +0100

    Change styling of directory listing

--- dir.go
+++ dir.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-  "os"
-  "log"
-  "bytes"
-  "strings"
-  "net/http"
+	"bytes"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func serveDir(w http.ResponseWriter, abs string, urlPath string) {
-  if !strings.HasSuffix(urlPath, "/") {
-    urlPath += "/"
-  }
-  w.Header().Add("Content-Type", "text/html")
+	if !strings.HasSuffix(urlPath, "/") {
+		urlPath += "/"
+	}
+	w.Header().Add("Content-Type", "text/html")
 
-  var b bytes.Buffer
-  dirEntries, err := os.ReadDir(abs)
-  if err != nil {
-    log.Println("error reading dir", abs)
-    return
-  }
-  
-  b.WriteString("<pre>\n")
-  for _, dirEntry := range dirEntries {
-    var optional string
-    if dirEntry.IsDir() {
-      optional = "/"
-    }
-    name := dirEntry.Name() + optional
-    strings.ReplaceAll(name, "\n", "")
-    b.WriteString("<a href=\"" + urlPath + name + "\">" + name + "</a>\n")
-  }
-  b.WriteString("</pre>\n")
+	var b bytes.Buffer
+	dirEntries, err := os.ReadDir(abs)
+	if err != nil {
+		log.Println("error reading dir", abs)
+		return
+	}
 
-  w.Write(c.header)
-  b.WriteTo(w)
-  w.Write(c.footer)
+	b.WriteString("<div class=\"entries\">")
+	for _, dirEntry := range dirEntries {
+		var optional string
+		if dirEntry.IsDir() {
+			optional = "/"
+		}
+		name := dirEntry.Name() + optional
+		strings.ReplaceAll(name, "\n", "")
+		b.WriteString("<a href=\"" + urlPath + name + "\">" + name + "</a><br>")
+	}
+	b.WriteString("</div>")
+
+	w.Write(c.header)
+	b.WriteTo(w)
+	w.Write(c.footer)
 }
